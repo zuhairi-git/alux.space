956ce3645e7014a57a09b866204f2100b0a8e928
ali-ui-dev <zohairi@live.com>
2025-04-17 20:47:19 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -14,7 +14,8 @@ const nextConfig = {
   },
   env: {
     NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000',
-  }
+  },
+  output: 'export', // Static export mode
 };
 
 module.exports = nextConfig;
