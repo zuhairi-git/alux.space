336c3fb45d2f9e82822f28c4ff43b0e46e1ab219
Ali Al-Zuhairi <eli.zuhairi@gmail.com>
2025-04-18 19:42:19 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    unoptimized: true, // <- disables image optimization for export mode
     remotePatterns: [
       {
         protocol: 'https',
